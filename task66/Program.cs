05244a9455fb0202a0d7697cc3ed21380498589c
Nikita <nikita.serg.utkin@gmail.com>
2022-12-19 23:26:43 +0900

    работает 66

---
+++ task66/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных 
+// элементов в промежутке от M до N.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+Console.Write("Введите натуральное число M: ");
+int M = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите натуральное число N большее M: ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine($"сумма чисел от {M} до {N} равна {SummaRec(M, N)}");
+int SummaRec(int M, int N)
+{
+    if (M == N) return M;
+    return N + (SummaRec(M, N - 1)); // в себе прячем sum+=N и N--
+}
+
